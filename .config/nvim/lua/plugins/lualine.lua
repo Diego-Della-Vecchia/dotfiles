f65141788ea64ff81a9d451bd1648ed6965a5d3f
Diego <diego.dellavecchia3@gmail.com>
2025-11-01 18:46:24 +0100

    Move tmux status bar to the top

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -5,6 +5,14 @@ return {
     require("lualine").setup({
       options = {
         theme = "catppuccin"
+      },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch' },
+        lualine_c = {},
+        lualine_x = { 'diagnostics', 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
       }
     })
   end
