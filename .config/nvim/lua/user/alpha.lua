6d43c2689ae2f9efac20932aa7a71d0c33fa072e
Diego Della Vecchia <diego.dellavecchia3@gmail.com>
2025-11-02 22:43:17 +0100

    fix alpha homescreen

--- .config/nvim/lua/user/alpha.lua
+++ .config/nvim/lua/user/alpha.lua
@@ -20,11 +20,11 @@ dashboard.section.buttons.val = {
   dashboard.button("f", "  Find File", ":Telescope find_files<CR>"),
   dashboard.button("g", "  Find Text", ":Telescope live_grep<CR>"),
   dashboard.button("n", "  New File", ":ene <BAR> startinsert<CR>"),
-  dashboard.button("s", "󰺄 Search sessions", ":AutoSession search<CR>"),
+  dashboard.button("s", "󰺄  Search sessions", ":AutoSession search<CR>"),
   dashboard.button("d", "  Delete sessions", ":AutoSession deletePicker<CR>"),
   dashboard.button("r", "  Recent", ":Telescope oldfiles<CR>"),
   dashboard.button("u", "  Update Plugins", ":Lazy update<CR>"),
-  dashboard.button("c", "⚙  Settings", ":e $MYVIMRC<CR>"),
+  dashboard.button("c", "  Settings", ":e $MYVIMRC<CR>"),
   dashboard.button("q", "⏻  Quit", ":qa<CR>"),
 }
 
