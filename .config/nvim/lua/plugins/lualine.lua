4a9e1e418233341b89f70bcd4d3006930977efdd
Diego <diego.dellavecchia3@gmail.com>
2025-11-02 19:22:07 +0100

    Customize lualine, add harpoon indicator

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,19 +1,35 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "catppuccin",
+  opts = {
+    options = {
+      theme = "catppuccin",
+      component_separators = { left = "", right = "" },
+      section_separators = { right = "", left = "" },
+    },
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch" },
+      lualine_c = {
+        "filename",
+        "%=",
+        {
+          "harpoon2",
+          icon = "󰛢",
+          indicators = { "a", "s", "q", "w" },
+          active_indicators = { "A", "S", "Q", "W" },
+          color_active = { fg = "#8caaee" },
+          _separator = " ",
+          no_harpoon = "No Harpoon",
+          align = "center",
+        },
       },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch" },
-        lualine_c = {},
-        lualine_x = { "diagnostics", "encoding", "fileformat", "filetype" },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-      },
-    })
+      lualine_x = { "diagnostics", { "fileformat", symbols = { unix = "" } }, "filetype" },
+      lualine_y = { "progress" },
+      lualine_z = { "location" },
+    },
+  },
+  config = function(_, opts)
+    require("lualine").setup(opts)
   end,
 }
