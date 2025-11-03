dd94884afe8ee6efa3a9c5803737f5b38ab11f2b
Diego Della Vecchia <diego.dellavecchia3@gmail.com>
2025-11-03 09:55:12 +0100

    fix timeout

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -17,7 +17,7 @@ return {
         yaml = { "prettierd", "prettier" },
       },
       format_on_save = {
-        timeout_ms = 500,
+        timeout_ms = 2500,
         lsp_fallback = true,
       },
     })
