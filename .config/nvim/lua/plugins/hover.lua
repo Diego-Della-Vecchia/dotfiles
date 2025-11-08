be2c5f63762d0a0e90d7722a7097236968e39816
Diego <diego.dellavecchia3@gmail.com>
2025-11-08 21:20:06 +0100

    fix hover.lua

--- .config/nvim/lua/plugins/hover.lua
+++ .config/nvim/lua/plugins/hover.lua
@@ -2,12 +2,15 @@ return {
   "lewis6991/hover.nvim",
   event = "BufReadPost", -- load after a buffer is opened
   config = function()
-    require("hover").setup({
-      init = function()
-        -- Require providers
-        require("hover.providers.lsp") -- lsp hover
-        -- You can also enable other providers like markdown, man, etc.
-      end,
+    require("hover").config({
+      providers = {
+        "hover.providers.diagnostic",
+        "hover.providers.lsp",
+        "hover.providers.man",
+      },
+      mouse_providers = {
+        "hover.providers.lsp",
+      },
       preview_opts = {
         border = nil, -- 'single', 'rounded', 'double', 'shadow', or nil
         max_width = 80,
@@ -17,7 +20,7 @@ return {
     })
 
     -- Keymaps
-    vim.keymap.set("n", "K", require("hover").hover, { desc = "Hover" })
-    vim.keymap.set("n", "gK", require("hover").hover_select, { desc = "Hover select" })
+    vim.keymap.set("n", "K", require("hover").open, { desc = "Hover" })
+    vim.keymap.set("n", "gK", require("hover").select, { desc = "Hover select" })
   end,
 }
