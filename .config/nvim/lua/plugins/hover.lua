8beda6060b6238289e1524c5932b95f8c9a33b63
Diego Della Vecchia <diego.dellavecchia3@gmail.com>
2025-11-28 18:52:04 +0100

    fix hover.lua

--- .config/nvim/lua/plugins/hover.lua
+++ .config/nvim/lua/plugins/hover.lua
@@ -8,19 +8,23 @@ return {
         "hover.providers.lsp",
         "hover.providers.man",
       },
+      select_providers = {
+        "hover.providers.diagnostic",
+        "hover.providers.lsp",
+        "hover.providers.man",
+      },
       mouse_providers = {
         "hover.providers.lsp",
       },
       preview_opts = {
-        border = nil, -- 'single', 'rounded', 'double', 'shadow', or nil
+        border = "rounded", -- 'single', 'rounded', 'double', 'shadow', or nil
         max_width = 80,
       },
-      -- Whether the hover window gets closed when moving the cursor
       quit_on_move = true,
     })
 
     -- Keymaps
     vim.keymap.set("n", "K", require("hover").open, { desc = "Hover" })
-    vim.keymap.set("n", "gK", require("hover").select, { desc = "Hover select" })
+    vim.keymap.set("n", "gK", require("hover").enter, { desc = "Hover select" })
   end,
 }
