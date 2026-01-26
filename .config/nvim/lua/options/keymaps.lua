b58826c3f762a5496ba6c10b04e3a032788f9712
Diego Della Vecchia <diego.dellavecchia3@gmail.com>
2026-01-26 19:42:37 +0100

    fix window splitting

--- .config/nvim/lua/options/keymaps.lua
+++ .config/nvim/lua/options/keymaps.lua
@@ -11,8 +11,8 @@ keymap.set("n", "<C-d>", "<C-d>zz")
 keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- Window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>sh", "<C-w>v", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>sv", "<C-w>s", { desc = "Split window vertically" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
